4f6eee1216a8119c0d608dd860e58f4e14c330cd
Kevin Lee <kevin.skyulee@gmail.com>
2021-11-29 00:17:33 -0500

    remove comment

--- src/components/Navbar/styles.jsx
+++ src/components/Navbar/styles.jsx
@@ -14,7 +14,6 @@ export const NavbarContainer = styled(AppBar)`
   @media (min-width: 1024px) {
     padding: 40px 170px 32px 170px;  
   }
-  // boxShadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%); */
 `
 
 export const Logo = styled('a')`
